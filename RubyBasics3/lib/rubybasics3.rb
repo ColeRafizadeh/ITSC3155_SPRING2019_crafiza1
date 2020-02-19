a9e9920be43c0d82e1d4f15d993352b3d98b9329
colerafi <crafiza1@uncc.edu>
2020-02-19 23:06:37 +0000

    Ruby 00 Basics - Part 3

--- RubyBasics3/lib/rubybasics3.rb
+++ RubyBasics3/lib/rubybasics3.rb
@@ -2,10 +2,15 @@
 
 class BookInStock
 attr_accessor :isbn, :price
-##
+#
   def initialize isbn, price
     raise ArgumentError if isbn.empty? || price <= 0
     @isbn = isbn
     @price = price
   end
+  
+  def price_as_string
+    format("$%.2f", @price)
+  end
+
 end
