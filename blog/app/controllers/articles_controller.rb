243a84a3b0e9ec4be0c165fd17a5c1f8fbcd454c
colerafi <crafiza1@uncc.edu>
2020-03-15 22:16:22 +0000

    added validations for title

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,19 +1,29 @@
 class ArticlesController < ApplicationController
+    
     def new
+        @article = Article.new
     end
+    
     def index
         @articles = Article.all
     end
+    
     def show
         @article = Article.find(params[:id])
     end
+    
     def create
         @article = Article.new(article_params)
         
-        @article.save
-        redirect_to @article
+        if @article.save
+            redirect_to @article
+        else
+            render 'new'
+        end
     end
+    
 end
+
 private
     def article_params
         params.require(:article).permit(:title, :text)
