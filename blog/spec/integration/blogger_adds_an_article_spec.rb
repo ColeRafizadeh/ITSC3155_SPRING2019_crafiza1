426b7f3ef403de8456796d65b261ed73aafa3154
colerafi <crafiza1@uncc.edu>
2020-03-26 03:42:17 +0000

    Add blogger adds an article scenario 2

--- blog/spec/integration/blogger_adds_an_article_spec.rb
+++ blog/spec/integration/blogger_adds_an_article_spec.rb
@@ -9,4 +9,13 @@ feature "blog adds an article" do
         expect(page).to have_field("Title")
         expect(page).to have_field("Text")
     end
+    scenario "Blogger successfully creates a new article" do
+        visit new_article_path
+        expect(page).to have_content("New Article")
+        fill_in "Title", with: "Capybabra Article"
+        fill_in "Text", with: "This is a new Capybara Article"
+        click_button "Create Article"
+        expect(page).to have_content("Capybabra Article")
+        expect(page).to have_content("This is a new Capybara Article")
+    end
 end
